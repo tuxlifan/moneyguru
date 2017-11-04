ab5a1c1e466ae80ba190f8c18cf5b91367dd6476
Virgil Dupras <hsoft@hardcoded.net>
2017-11-04 13:42:36 -0400

    Disable yahoo currency plugin
    The service doesn't exist anymore.

--- core/plugin/yahoo_currency_provider.py
+++ core/plugin/yahoo_currency_provider.py
@@ -11,12 +11,9 @@ from datetime import date
 
 from core.plugin import CurrencyProviderPlugin
 
-# This is the exception we have to raise when, for some reason, we temporarily can't contact the
-# rate provider.
-from core.model.currency import RateProviderUnavailable
-
-# We use Python's built-in urlopen to hit Yahoo's server and fetch the rates
-from urllib.request import urlopen
+# SERVICE DOESN'T EXIST ANYMORE
+# Yahoo stopped serving currency rates at the address we were using. We keep the plugin around for
+# the currencies it defines, but otherwise, this plugin is now useless.
 
 # To create a currency provider plugin, one must subclass CurrencyProviderPlugin. You can see more
 # details about how to subclass it in plugins.py. It's accessible online at:
@@ -61,19 +58,3 @@ class YahooProviderPlugin(CurrencyProviderPlugin):
             ('XAU', 'Gold (ounce)', 2, 1430.39),
             ('XAG', 'Silver (ounce)', 2, 23.13),
         ]
-
-    # Then, we must implement the rate fetching method. It has to return a float rate that is the
-    # value, today of 1 "currency_code" in CAD (Canadian Dollars).
-    # This method is called asynchronously, it will not block moneyGuru if it takes a long time to
-    # resolve.
-    def get_currency_rate_today(self, currency_code):
-        # the result of this request is a single CSV line like this:
-        # "CADBHD=X",0.3173,"11/7/2008","5:11pm",N/A,N/A,N/A,N/A,N/A
-        try:
-            url = 'http://download.finance.yahoo.com/d/quotes.csv?s=%sCAD=X&f=sl1d1t1c1ohgv&e=.csv' % currency_code
-            with urlopen(url, timeout=10) as response:
-                content = response.read().decode('latin-1')
-            return float(content.split(',')[1])
-        except Exception:
-            raise RateProviderUnavailable()
-
--- core/tests/model/currency_test.py
+++ core/tests/model/currency_test.py
@@ -13,7 +13,7 @@ from hscommon.testutil import jointhreads, eq_
 from ...model.amount import convert_amount
 from ...model.amount import Amount
 from ...model.currency import Currency, USD, CAD, RateProviderUnavailable, RatesDB
-from ...plugin import yahoo_currency_provider, boc_currency_provider
+from ...plugin import boc_currency_provider
 
 def slow_down(func):
     def wrapper(*args, **kwargs):
@@ -114,25 +114,17 @@ def test_no_internet(monkeypatch):
     # No crash occur if the computer don't have access to internet.
     from socket import gaierror
     monkeypatch.setattr(boc_currency_provider, 'urlopen', exception_raiser(gaierror()))
-    monkeypatch.setattr(yahoo_currency_provider, 'urlopen', exception_raiser(gaierror()))
     with raises(RateProviderUnavailable):
         boc_currency_provider.BOCProviderPlugin().wrapped_get_currency_rates(
             'USD', date(2008, 5, 20), date(2008, 5, 20)
-        )
-        yahoo_currency_provider.YahooProviderPlugin().wrapped_get_currency_rates(
-            'LVL', date(2008, 5, 20), date(2008, 5, 20)
         )
 
 def test_connection_timeout(monkeypatch):
     # No crash occur the connection times out.
     from socket import error
     monkeypatch.setattr(boc_currency_provider, 'urlopen', exception_raiser(error()))
-    monkeypatch.setattr(yahoo_currency_provider, 'urlopen', exception_raiser(error()))
     with raises(RateProviderUnavailable):
         boc_currency_provider.BOCProviderPlugin().wrapped_get_currency_rates(
             'USD', date(2008, 5, 20), date(2008, 5, 20)
-        )
-        yahoo_currency_provider.YahooProviderPlugin().wrapped_get_currency_rates(
-            'LVL', date(2008, 5, 20), date(2008, 5, 20)
         )
 
